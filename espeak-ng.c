2229f206d19e6c5e603fe2a895a7430e31d4c4ee
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2021-12-19 22:03:51 +0100

    french voice

--- espeak-ng.c
+++ espeak-ng.c
@@ -7,11 +7,23 @@ const int buflength = 500, options = 0;
 const unsigned int position = 0, position_type = 0, end_position = 0, flags = espeakCHARS_AUTO;
 void* user_data;
 unsigned int *identifier;
+espeak_VOICE voice;
+const char *langNativeString = "fr";
+int is_init = 0;
 
 static Janet init(int32_t argc, Janet *argv) {
     janet_fixarity(argc, 0);
+    if(is_init)
+      janet_panic("espeak-ng already initialized");
+    else
+      is_init = 1;
     espeak_Initialize(output, buflength, path, options);
-    espeak_SetVoiceByName("English");
+    memset(&voice, 0, sizeof(espeak_VOICE));
+    voice.languages = langNativeString;
+    voice.name = "FR";
+    voice.variant = 2;
+    voice.gender = 2;
+      espeak_SetVoiceByProperties(&voice);
     return janet_wrap_nil();
 }
 
